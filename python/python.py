b53a62e836fe5def2934b00fac32fdcb8890d0ad
Amina Farouk <adoameena@gmail.com>
2023-01-26 14:54:03 +0100

    exercise

---
+++ python/python.py
@@ -0,0 +1,12 @@
+print ("Hello, User")
+
+name = input("What is your name: ")
+print(f"Welcome, {name},")
+print ("Thank You!")
+
+print ("I am studying Information and Communication Technology")
+
+color = input("What is your favorite color?: ")
+print(f"My favorite color is {color}")
+
+
