0bd4b9d631eeba27a81e48bb60f41501a9da6f0e
Amina Farouk <adoameena@gmail.com>
2023-03-02 11:05:18 +0100

    tuples

---
+++ DSA/tuples.py
@@ -0,0 +1,30 @@
+#a = (1, 2, 3, 4, 5)
+#for i in a:
+    #print(i)
+
+#    print(a[0])
+
+#a = {True, 1, 2, 4, 5}
+#for i in a:
+    #print(i) 
+#a = 3
+#b = 2
+#c = 6
+
+#if a < b:
+#    print('Esther')
+#elif a == b & a > b:
+#    print('Halima')
+#elif a == b or a < c:
+#    print('Amina')
+#else:
+#    print("Abbas")
+
+h = [1,2,3,4,5]
+h.append([1,2, 3])
+
+
+print(h)
+
+for name in h:
+    print (name)
