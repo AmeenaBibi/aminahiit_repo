98313a1271e8cc3c0e5a78e4f7128d4daf8c3cc8
Amina Farouk <adoameena@gmail.com>
2023-03-13 11:44:39 +0100

    commit

---
+++ file_handling/file_handling.py
@@ -0,0 +1,15 @@
+# f = open("test.txt")
+
+# print(f.readlines()[])
+
+# f = open("work.text", "r+")
+
+# a = f.read()
+# f.write('hello \n')
+# print(a)
+
+# f = open('file.txt', 'w+')
+# f.write('Hello world\n')
+# f.writelines(['Testing\n', 'You are welcome to ecc\n'])
+# print(f.write())
+# print(f.read())
