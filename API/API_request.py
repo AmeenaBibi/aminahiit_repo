4e8f6a63d277bcd00c2a321b2edd63fc1ca184e3
Amina Farouk <adoameena@gmail.com>
2023-03-22 11:54:46 +0100

    commit

--- API/API_request.py
+++ API/API_request.py
@@ -1,6 +1,23 @@
-import requests
-response = requests.get("http://api.weatherapi.com/v1/current.json?key=7f58f399e78f430b92195359232103&q=Abuja&aqi=no")
+# import requests
+# response = requests.get("http://api.weatherapi.com/v1/current.json?key=7f58f399e78f430b92195359232103&q=Abuja&aqi=no")
 
-# print(response.json())
-data = response.json()
-print(data['location']['localtime'])
+# # print(response.json())
+# data = response.json()
+# print(data['location']['localtime'])
+
+# import requests
+# response = requests.get("https://dummyjson.com/products")
+
+# data = response.json()
+# print(data['products'][1]['images'][1])
+
+# a = ['a', {4: 'books', 'b': ['hand', 'toes', 'legs', {'c': 'pencil'}, 'head']}]
+# print(a[1]['b'][4])
+
+import requests
+
+url = "https://dummyjson.com/products/1"
+json = {'title': 'Harry Potter', 'description': 'This is a book', 'stock': 69}
+response = requests.put(url, data=json)
+
+print(response.json())
